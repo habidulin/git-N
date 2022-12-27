f2d36a8c9f0b77c4edc35521c7aa740b5c271055
habidulin <111412489+habidulin@users.noreply.github.com>
2022-12-27 17:53:54 +0100

    1.3

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-info jast for chek stat - 1
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+var cursor3 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", function (e) {
+  cursor.style.cssText = cursor2.style.cssText = cursor3.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+  
+});
